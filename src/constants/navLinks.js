b3a2c372875aad6399f91c26670eb8ba7ec71226
romanzaman23-cmyk <romanzaman23@gmail.com>
2026-09-02 11:48:01 +0300

    chore: remove redundant legacy root files after frontend-backend separation

--- src/constants/navLinks.js
+++
@@ -1,6 +0,0 @@
-export {
-  serviceSlugMap as serviceLinks,
-  mainNavKeys as mainNavLinks,
-} from '../i18n/translations'
-
-export { scrollToSection, handleNavClick, openServiceFromNav, closeModalAndScrollTo } from '../utils/scroll'
